727a0062d56993031469bf262b9115dd0e57f676
David Faure <faure@kde.org>
2008-03-25 20:21:53 +0000

    Someone asked me on IRC some time ago how to locate an icon from the command line, here it is. Can be useful for debugging, too (kiconloader hackers, icon theme writers, artists, app writers etc.).
    svn path=/trunk/KDE/kdebase/runtime/; revision=790080

---
+++ src/tools/kiconfinder/kiconfinder.cpp
@@ -0,0 +1,55 @@
+/*  This file is part of the KDE project
+    Copyright (C) 2008 David Faure <faure@kde.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License or ( at
+    your option ) version 3 or, at the discretion of KDE e.V. ( which shall
+    act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; see the file COPYING.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include <kcmdlineargs.h>
+#include <kiconloader.h>
+#include <kdeversion.h>
+#include <kcomponentdata.h>
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    KCmdLineArgs::init( argc, argv, "kiconfinder", 0, ki18n("Icon Finder"), KDE_VERSION_STRING , ki18n("Finds an icon based on its name"));
+
+
+    KCmdLineOptions options;
+
+    options.add("+iconname", ki18n("the iconname to look for"));
+
+    KCmdLineArgs::addCmdLineOptions( options );
+
+    KComponentData instance("kicontypefinder");
+
+    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+    if( args->count() < 1 ) {
+        printf( "No icon name specified\n" );
+        return 1;
+    }
+    const QString iconName = args->arg( 0 );
+    QString icon = KIconLoader::global()->iconPath(iconName, KIconLoader::Desktop /*TODO configurable*/, true);
+    if ( !icon.isEmpty() ) {
+        printf("%s\n", icon.toLatin1().constData());
+    } else {
+        return 1; // error
+    }
+
+    return 0;
+}
