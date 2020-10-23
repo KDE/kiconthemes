b33c69928c6c42ba9a42e4b152a0cd6971b9ecf7
Hannah von Reth <vonreth@kde.org>
2020-10-23 15:51:52 +0200

    Add Windows support to ksvg2icns
    Allow to create windows .ico files.
    Also add --legacy-png mode, which allows to generate the legacy png files used by current versions of ecm.

--- src/tools/ksvg2icns/ksvg2icns.cpp
+++ src/tools/ksvg2icns/ksvg2icns.cpp
@@ -59,12 +59,14 @@ int main(int argc, char *argv[])
     QCommandLineParser parser;
     parser.setApplicationDescription(app.translate("main", "Creates an icns file from an svg image"));
     parser.addPositionalArgument("iconname", app.translate("main", "The svg icon to convert"));
+    QCommandLineOption pngOnly(QStringLiteral("legacy-png"), QStringLiteral("Only create pngs used by the legacy ecm code"));
+    parser.addOption(pngOnly);
     parser.addHelpOption();
 
     parser.process(app);
+
     if (parser.positionalArguments().isEmpty()) {
-        parser.showHelp();
-        return 1;
+        parser.showHelp(1);
     }
 
     bool isOk;
@@ -80,20 +82,19 @@ int main(int argc, char *argv[])
     isOk = QDir(tmpDir.path()).mkpath(outPath);
     EXIT_ON_ERROR(isOk, "Unable to create out.iconset directory\n");
 
-    const QStringList &args = app.arguments();
-    EXIT_ON_ERROR(args.size() == 2,
-                  "Usage: %s svg-image\n",
-                  qPrintable(args.value(0)));
-
-    const QString &svgFileName = args.at(1);
+    const QStringList &args = parser.positionalArguments();
+    if(args.size() != 1) {
+        parser.showHelp(1);
+    }
+    const QString &svgFileName = args.at(0);
 
     // open the actual svg file
     QSvgRenderer svg;
     isOk = svg.load(svgFileName);
     EXIT_ON_ERROR(isOk, "Unable to load %s\n", qPrintable(svgFileName));
 
-    // The sizes are from:
-    // https://developer.apple.com/library/mac/documentation/GraphicsAnimation/Conceptual/HighResolutionOSX/Optimizing/Optimizing.html
+
+    const QString baseName = QFileInfo(svgFileName).baseName();
 
     struct OutFiles
     {
@@ -102,16 +103,64 @@ int main(int argc, char *argv[])
         QString out2;
     };
 
-    // create the pngs in various resolutions
-    const OutFiles outFiles[] = {
-        { 1024, outPath + QStringLiteral("/icon_512x512@2x.png"), QString() },
-        {  512, outPath + QStringLiteral("/icon_512x512.png"),    outPath + QStringLiteral("/icon_256x256@2x.png") },
-        {  256, outPath + QStringLiteral("/icon_256x256.png"),    outPath + QStringLiteral("/icon_128x128@2x.png") },
-        {  128, outPath + QStringLiteral("/icon_128x128.png"),    QString() },
-        {   64, outPath + QStringLiteral("/icon_32x32@2x.png"),   QString() },
-        {   32, outPath + QStringLiteral("/icon_32x32.png"),      outPath + QStringLiteral("/icon_16x16@2x.png") },
-        {   16, outPath + QStringLiteral("/icon_16x16.png"),      QString() }
+    auto addLegacyIcon = [&](int size, bool createHDPI = false,  const QString &_infix = QString()) -> OutFiles {
+        const QString path = parser.isSet(pngOnly) ? QStringLiteral(".") : outPath;
+        const QString infix = _infix.isEmpty() ? QString() : QStringLiteral("-%1").arg(_infix);
+        return { size,
+                QStringLiteral("%1/%2-%3%4.png").arg(path, QString::number(size), baseName,  infix),
+                createHDPI ? QStringLiteral("%1/%2@2x-%3%4.png").arg(path, QString::number(size / 2), baseName, infix) : QString()
+        };
     };
+
+    // create the pngs in various resolutions
+    QVector<OutFiles> outFiles;
+    if (parser.isSet(pngOnly)) {
+        // based on https://invent.kde.org/frameworks/extra-cmake-modules/-/blob/master/modules/ECMAddAppIcon.cmake
+        const QString sidebar = QStringLiteral("sidebar");
+        outFiles = {
+            { addLegacyIcon(1024, false, sidebar) },
+            { addLegacyIcon( 512, false, sidebar) },
+            { addLegacyIcon( 256, false, sidebar) },
+            { addLegacyIcon( 128, false, sidebar) },
+            { addLegacyIcon(  64, false, sidebar) },
+            { addLegacyIcon(  32, false, sidebar) },
+            { addLegacyIcon(  16, false, sidebar) },
+            { addLegacyIcon(1024, true) },
+            { addLegacyIcon( 256, true) },
+            { addLegacyIcon( 128)       },
+            { addLegacyIcon(  64)       },
+            { addLegacyIcon(  48)       },
+            { addLegacyIcon(  32, true) },
+            { addLegacyIcon(  24)       },
+            { addLegacyIcon(  16)       }
+        };
+    } else {
+#ifndef Q_OS_WIN
+        // The sizes are from:
+        // https://developer.apple.com/library/mac/documentation/GraphicsAnimation/Conceptual/HighResolutionOSX/Optimizing/Optimizing.html
+        outFiles= {
+            { 1024, outPath + QStringLiteral("/icon_512x512@2x.png"), QString() },
+            {  512, outPath + QStringLiteral("/icon_512x512.png"),    outPath + QStringLiteral("/icon_256x256@2x.png") },
+            {  256, outPath + QStringLiteral("/icon_256x256.png"),    outPath + QStringLiteral("/icon_128x128@2x.png") },
+            {  128, outPath + QStringLiteral("/icon_128x128.png"),    QString() },
+            {   64, outPath + QStringLiteral("/icon_32x32@2x.png"),   QString() },
+            {   32, outPath + QStringLiteral("/icon_32x32.png"),      outPath + QStringLiteral("/icon_16x16@2x.png") },
+            {   16, outPath + QStringLiteral("/icon_16x16.png"),      QString() }
+        };
+#else
+        // According to https://stackoverflow.com/a/40851713/2886832
+        // Windows always chooses the first icon above 255px, all other ones will be ignored
+        outFiles = {
+            { addLegacyIcon(1024) },
+            { addLegacyIcon( 128) },
+            { addLegacyIcon(  64) },
+            { addLegacyIcon(  48) },
+            { addLegacyIcon(  32) },
+            { addLegacyIcon(  24) },
+            { addLegacyIcon(  16) }
+        };
+#endif
+    }
 
     for (const OutFiles &outFile : outFiles) {
         isOk = writeImage(svg, outFile.size, outFile.out1, outFile.out2);
@@ -120,23 +169,43 @@ int main(int argc, char *argv[])
         }
     }
 
-    // convert the iconset to icns using the "iconutil" command
+    if (!parser.isSet(pngOnly)) {
+        // convert the iconset to icns using the "iconutil" command
+#ifndef Q_OS_WIN
+        const QString tool = QStringLiteral("iconutil");
+        const QString outIcns = baseName + QStringLiteral(".icns");
 
-    const QString outIcns = QFileInfo(svgFileName).baseName() + QStringLiteral(".icns");
+        const QStringList utilArgs = {
+            QStringLiteral("-c"), QStringLiteral("icns"),
+            QStringLiteral("-o", outIcns,
+            outPath
+        };
+#else
+        const QString tool = QStringLiteral("icotool");
+        const QString outIco = baseName + QStringLiteral(".ico");
 
-    const QStringList utilArgs = QStringList()
-            << "-c" << "icns"
-            << "-o" << outIcns
-            << outPath;
+        QStringList utilArgs = {
+            QStringLiteral("-c"),
+            QStringLiteral("-o"), outIco
+        };
+        for (const auto &f : outFiles) {
+            if (f.size == 1024) {
+                utilArgs << QStringLiteral("-r");
+            }
+            utilArgs << f.out1;
+        }
+#endif
 
-    QProcess iconUtil;
+        QProcess iconUtil;
 
-    iconUtil.start(QStringLiteral("iconutil"), utilArgs, QIODevice::ReadOnly);
-    isOk = iconUtil.waitForFinished(-1);
-    EXIT_ON_ERROR(isOk, "Unable to launch iconutil: %s\n", qPrintable(iconUtil.errorString()));
+        iconUtil.start(tool, utilArgs, QIODevice::ReadOnly);
+        isOk = iconUtil.waitForFinished(-1);
+        EXIT_ON_ERROR(isOk, "Unable to launch %s: %s\n", qPrintable(tool), qPrintable(iconUtil.errorString()));
 
-    EXIT_ON_ERROR(iconUtil.exitStatus() == QProcess::NormalExit, "iconutil crashed!\n");
-    EXIT_ON_ERROR(iconUtil.exitCode() == 0, "iconutil returned %d\n", iconUtil.exitCode());
+        EXIT_ON_ERROR(iconUtil.exitStatus() == QProcess::NormalExit, "%s crashed!\n", qPrintable(tool));
+        EXIT_ON_ERROR(iconUtil.exitCode() == 0, "%s returned %d\n", qPrintable(tool), iconUtil.exitCode());
+    }
+
 
     return 0;
 }
