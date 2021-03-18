dd6fad78caaa0d1791bcc4a6675940cf4daf9f86
Christoph Cullmann <cullmann@kde.org>
2021-03-18 20:59:47 +0100

    assert hicolor default is always found
    we bundle a default .theme file inside the library
    resources
    
    this can't fail as fallback
    
    instead of allowing some "bad" state, just assert
    this

--- src/kiconloader.cpp
+++ src/kiconloader.cpp
@@ -641,12 +641,10 @@ void KIconLoaderPrivate::initIconThemes()
         delete def;
         // warn, as this is actually a small penalty hit
         qCDebug(KICONTHEMES) << "Couldn't find current icon theme, falling back to default.";
+
+        // this can't fail, as defaultThemeName() == hicolor is in a resource of this library itself as last fallback!
         def = new KIconTheme(KIconTheme::defaultThemeName(), appname);
-        if (!def->isValid()) {
-            qCDebug(KICONTHEMES) << "Standard icon theme" << KIconTheme::defaultThemeName() << "not found!";
-            delete def;
-            return;
-        }
+        Q_ASSERT(def->isValid());
     }
     mpThemeRoot = new KIconThemeNode(def);
     mThemesInTree.append(def->internalName());
