ef3cbe0f711a9245b7fa3ae0fd251316cc190f6d
Tobias Fella <tobias.fella@gnupg.com>
2024-02-20 10:48:02 +0100

    Mark index.theme config as SimpleConfig
    There is no apparent need for cascading

--- src/kicontheme.cpp
+++ src/kicontheme.cpp
@@ -363,7 +363,7 @@ KIconTheme::KIconTheme(const QString &name, const QString &appName, const QStrin
 
     // Use KSharedConfig to avoid parsing the file many times, from each component.
     // Need to keep a ref to it to make this useful
-    d->sharedConfig = KSharedConfig::openConfig(fileName, KConfig::NoGlobals);
+    d->sharedConfig = KSharedConfig::openConfig(fileName, KConfig::SimpleConfig);
 
     KConfigGroup cfg(d->sharedConfig, mainSection);
     d->mName = cfg.readEntry("Name");
