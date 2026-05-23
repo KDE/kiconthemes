c0ff47ce81057b515b52188fad36553460959d53
Friedrich W. H. Kossebau <kossebau@kde.org>
2026-05-23 17:51:55 +0200

    Add notes to drop dependency on KWidgetsAddons for KF7

--- src/widgets/kpixmapsequenceloader.cpp
+++ src/widgets/kpixmapsequenceloader.cpp
@@ -18,3 +18,13 @@ KPixmapSequence load(const QString &iconName, int size)
 }
 
 }
+
+#if KICONTHEMES_VERSION >= QT_VERSION_CHECK(6, 240, 0)
+// The include of <KPixmapSequence> needs public linkage to KWidgetsAddons of KIconWidgets,
+// just for this one util method (which tries to hide the KIconLoader complexity).
+// Also the current (wrong) private linkage of KWidgetsAddons pulls in that runtime dependency for
+// any other consumers of KIconWidgets.
+// So instead providing the dependency is better left just to the users of this util method,
+// who will need KWidgetsAddons anyway to handle the returned KPixmapSequence instance.
+#pragma message("Make KPixmapSequenceLoader::load() inline header-only, drop linking KWidgetsAddons (cmp. KQuickIconProvider)")
+#endif
